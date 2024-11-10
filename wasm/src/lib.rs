38ce5d618ef7e7a54cbe823e94518b787af76488
em 🐈‍⬛ <146671001+empxor@users.noreply.github.com>
2024-11-09 17:52:51 -0800

    fix binding warning

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -91,6 +91,5 @@ pub fn convert_dollcode(input: &str) -> Result<JsValue, JsValue> {
 // Initialize panic hook
 #[wasm_bindgen(start)]
 pub fn init() {
-    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
